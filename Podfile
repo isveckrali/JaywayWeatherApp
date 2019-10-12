85a1cb0ac2c0ebb0bec37179400602f3c3b43d83
Mehmet Can Seyhan <mehmet.can.seyhan1@gmail.com>
2019-10-12 03:09:39 +0200

    trying 2

--- Podfile
+++ Podfile
@@ -5,8 +5,6 @@ target 'WeatherAppCovrChallenge' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
   pod 'Alamofire'
-  pod 'SCLAlertView'
-  pod 'Disk'
   # Pods for WeatherAppCovrChallenge
 
 end
--- WeatherAppCovrChallenge/Contoller/WeatherDetailViewController/WeatherDetailVC.swift
+++ WeatherAppCovrChallenge/Contoller/WeatherDetailViewController/WeatherDetailVC.swift
@@ -8,112 +8,83 @@
 
 import UIKit
 import Alamofire
-import Disk
 
-class WeatherDetailVC: BaseVC {
-
+class WeatherDetailVC: BaseVC, UITableViewDelegate, UITableViewDataSource {
+ 
     //IBOutlets
-    @IBOutlet var lblName: UILabel!
-    @IBOutlet var lblTemperature: UILabel!
-    @IBOutlet var lblWind: UILabel!
-    @IBOutlet var lblSummary: UILabel!
-    @IBOutlet var lblHumidity: UILabel!
-    @IBOutlet var imgViewIcon: UIImageView!
+    @IBOutlet var tableViewWeatherDetail: UITableView!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
     //Variables
-    var cityId:Int = 0
-    var cityLat:Int = 0
-    var cityLon:Int = 0
-    var cityName:String = ""
+    var cityId:Int?
+    var cityName:String?
     var weatherDetailModel: WeatherDetailModel?
     var listViewDetailDelegate:ListViewDetailDelegate!
     
-    //Constant
-    let BASE_CACHE_PATH:String = ""
+    //Constants
+    let DETAIL_TABLE_VIEW_REUSE_IDENTIFIER:String = "cellDetailTableView"
     
     //Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.lblName.text = cityName
-         preConditions()
         // Do any additional setup after loading the view.
+        setFirstValueAndCallFuncs()
     }
     
-    @IBAction func btnRefreshClicked(_ sender: UIButton) {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return weatherDetailModel?.list?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: DETAIL_TABLE_VIEW_REUSE_IDENTIFIER) as! WeatherDatailCell
+        cell.list = WeatherDetailViewModel(list: weatherDetailModel?.list![indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    //set first values and call functions
+    func setFirstValueAndCallFuncs() {
+        self.title = cityName
         preConditions()
     }
     
+ 
+    //check whether city id is empty
     func preConditions() {
-        if cityId != 0 {
-            checkInternetConnectionAndGetData()
-        } else {
-            getLocalData()
+        guard cityId != nil else {
+            self.navigationController?.popViewController(animated: true)
+            self.listViewDetailDelegate.createAlert(message: "City id is empty", title: "Warning")
+            //call prepage and crate alert view to user for empty id
+            return
         }
+         checkInternetConnectionAndGetData()
     }
     
+    //Get data from server or close page and show alert to user
     func checkInternetConnectionAndGetData() {
-        if Reachability.isConnectedToNetwork() {
-            getDataFromServer(url: (Services.BASE_URL + cityId.description + "&appid=" + Services.API_KEY))
+        if InternetController.isConnectedToNetwork() {
+            getDataFromServer(url: (Services.WEATHER_DETAIL_URL + cityId!.description + "&appid=" + Services.API_KEY))
         } else {
-            if existLocalData() {
-                self.getLocalData()
-            } else {
                 self.navigationController?.popViewController(animated: true)
                 self.listViewDetailDelegate.createAlert(message: "Please check your internet connection", title: "Warning")
-            }
         }
     }
     
-    func setData() {
-        if let mainModel = weatherDetailModel?.main {
-            self.lblTemperature.text = (mainModel.temp?.description ?? "") + "°"
-            self.lblHumidity.text = (mainModel.humidity?.description ?? "") + "%"
-        }
-        if let weatherModel = weatherDetailModel?.weather {
-            self.lblSummary.text = weatherModel[0].description ?? ""
-            self.imgViewIcon.image = weatherModel[0].iconImage
-        }
-        if let windModel = weatherDetailModel?.wind {
-            self.lblWind.text = (windModel.speed?.description ?? "") + "%"
-        }
-    }
     
-    func existLocalData() -> Bool{
-        return Disk.exists(BASE_CACHE_PATH + "/" + self.cityId.description, in: .caches)
-    }
-    
-    func saveLocalData() {
-        do {
-         try Disk.save(self.weatherDetailModel, to: .caches, as: BASE_CACHE_PATH + "" + self.cityId.description)
-            self.setData()
-            self.activityIndicator.stopAnimating()
-        } catch {
-            //print(error.localizedDescription)
-        }
-    }
-    
-    func getLocalData() {
-        do {
-          self.weatherDetailModel =  try Disk.retrieve(BASE_CACHE_PATH + "" + self.cityId.description, from: .caches, as: WeatherDetailModel.self)
-            setData()
-        } catch {
-            self.listViewDetailDelegate.createAlert(message: error.localizedDescription, title: "Warning")
-            //print(error.localizedDescription)
-        }
-         self.activityIndicator.stopAnimating()
-    }
-    
+    //Get data from server.
     func getDataFromServer(url:String) {
         Alamofire.request(url).responseJSON { (dataResponse) in
             self.activityIndicator.stopAnimating()
             if dataResponse.result.isSuccess {
                 do {
                     self.weatherDetailModel = try JSONDecoder().decode(WeatherDetailModel.self, from: dataResponse.data!)
-                    self.setData()
-                    self.saveLocalData()
+                    self.tableViewWeatherDetail.reloadData()
                 } catch {
                     //print(error.localizedDescription)
+                    self.navigationController?.popViewController(animated: true)
                     self.listViewDetailDelegate.createAlert(message: error.localizedDescription, title: "Warning")
                 }
             } else {
@@ -121,4 +92,6 @@ class WeatherDetailVC: BaseVC {
             }
         }
     }
+    
+    
 }
